c1f601052777a663f08bcdd5c886a9bc818db5b7
Jay <j.thomastt1@gmail.com>
2022-10-27 14:29:47 -0600

    forgot to commit / added css. questions scores, intials promptand event listener

--- Assets/JS/script.js
+++ Assets/JS/script.js
@@ -1,10 +1,9 @@
-var startQuizEl = document.getElementById('btn');
-var highScoresEl = document.getElementById('btn2');
 
+document.getElementById("start-quiz").addEventListener("click", quizStart);
 
+function quizStart() {
 
-//Counter 
-var count = 60 
+var count = 15
 var countdown = function (){
 (count);
 count--;
@@ -15,3 +14,38 @@ clearInterval(startCountdown);
 };
 
 var startCountdown = setInterval(countdown, 1000);
+
+
+
+var questions = [
+    {
+        prompt: "Commonly used data types DO NOT include.\n(a) Strings\n\ (b) Bolleans\n (c) Alerts",
+        answer: "c"
+    },
+
+    {
+       prompt: "The condition in a IF / ELSE statemenmt is enclosed with_______.\n(a) Quotes\n\ (b) Curly Brackets\n (c) Parenthesis",
+        answer: "b"
+    },
+
+    {
+        prompt: "Arrays can be used to store_____.\n(a) other arrays\n\ (b) Local Storage\n (c) All of the above",
+        answer: "c"
+    },
+] 
+
+var highScore = 0;
+
+for(var i=0; i < questions.length; i++){
+    var response = prompt(questions[i].prompt)
+    if(response == questions[i].answer){
+        highScore++;
+        alert("Correct Answer!");
+    } else {
+        alert("WRONG Answer!");
+    }
+}alert("Your Results: " + highScore + "/" + questions.length + intials);
+};
+
+
+var intials = prompt("your intials");
